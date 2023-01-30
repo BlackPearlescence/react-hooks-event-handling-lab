c9dbdbebcc1ddb33228167438ceaf3703b26d7b0
Sani Rogers <sanirogers138@gmail.com>
2023-01-29 22:54:17 -0500

    done react event handling

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    function focusText(e){
+        console.log("Good!");
+    }
+
+    function blurText(e){
+        console.log("Hey! Eyes on me!");
+    }
+
+    return(
+        <button onFocus={focusText} onBlur={blurText}>{"Eyes on me"}</button>
+    );
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function pwdMsg(e){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={pwdMsg}/>
+        </div>
     )
 }
 
